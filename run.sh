3de36f9a511ac6f65d7b7ab8cd0728d6dd143542
HawkliiX <gavinhawk716@gmail.com>
2018-07-27 16:48:06 -0700

    Some Fixes in code

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-root=$(pwd)
-export PYTHONIOENCODING=utf8
-echo 'Checking Dependencies...'
-python3 -m pip install --user --upgrade -r requirements.txt --quiet
-echo 'Loading Bot...'
-cd "$root/bot"
-python3 ./main.py
